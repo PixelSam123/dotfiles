a35ddb22960d793384fcf0a1555edfc1f0b2e54b
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-07-09 17:08:57 +0700

    play sound in ptt toggle script

--- scripts/ptt-toggle.sh
+++ scripts/ptt-toggle.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 if [ $(< ./is-ptt-active.txt) = true ]; then
+    aplay ./ptt-off.wav &
+
     echo "false" > ./is-ptt-active.txt
     pactl set-source-mute 0 0
     pactl set-sink-volume 0 $(< ./last-volume.txt)
 else
+    aplay ./ptt-on.wav &
+
     echo "true" > ./is-ptt-active.txt
     pactl get-sink-volume 0 | grep -Po "\\d+%" | head -n 1 > ./last-volume.txt
 
