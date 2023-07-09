c2b997be30bcf9d9579477fbde4e6b316fe96997
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-07-09 17:13:24 +0700

    fix flipped logic in ptt toggle mic muting

--- scripts/ptt-toggle.sh
+++ scripts/ptt-toggle.sh
@@ -4,7 +4,7 @@ if [ $(< ./is-ptt-active.txt) = true ]; then
     aplay ./ptt-off.wav &
 
     echo "false" > ./is-ptt-active.txt
-    pactl set-source-mute 0 0
+    pactl set-source-mute 0 1
     pactl set-sink-volume 0 $(< ./last-volume.txt)
 else
     aplay ./ptt-on.wav &
@@ -13,5 +13,5 @@ else
     pactl get-sink-volume 0 | grep -Po "\\d+%" | head -n 1 > ./last-volume.txt
 
     pactl set-sink-volume 0 10%
-    pactl set-source-mute 0 1
+    pactl set-source-mute 0 0
 fi
