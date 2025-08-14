2d1dd8c8a1367f644c21a2db4b8e6acff6b28b4f
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2025-08-14 19:20:43 +0700

    try to improve vmware nixos config

--- .config/nixos/per-instance/vmware.nix
+++ .config/nixos/per-instance/vmware.nix
@@ -1,6 +1,9 @@
 {
   networking.hostName = "sam-nixos-vmware"; # Define your hostname.
 
+  # Use vmware video driver
+  services.xserver.videoDrivers = [ "vmware" ];
+
   # Enable the VMWare Guest Agent.
   virtualisation.vmware.guest.enable = true;
 }
