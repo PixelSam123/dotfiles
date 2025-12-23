5d1d28d57a47c84fc445305ef1e62c331046c10f
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2025-12-23 14:33:28 +0700

    add lazyvim config

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,13 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local opt = vim.opt
+
+opt.guifont = jit.os == "Windows" and "Iosevka SS03,Iosevka Nerd Font:h12"
+  or jit.os == "OSX" and "MonaspiceNe Nerd Font:h16"
+  or "Iosevka SS09,Iosevka Nerd Font:h20"
+
+opt.shell = jit.os == "Windows" and "nu" or opt.shell
+opt.shellcmdflag = jit.os == "Windows" and "-c" or opt.shellcmdflag
+opt.shellxquote = jit.os == "Windows" and "" or opt.shellxquote
