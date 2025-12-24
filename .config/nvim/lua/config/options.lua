9c04e835ecdb13c46ff10859893d326ce2219a2f
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2025-12-24 17:16:06 +0700

    change font size in nvim config

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,10 +3,11 @@
 -- Add any additional options here
 
 local opt = vim.opt
+local env = vim.env
 
 opt.guifont = jit.os == "Windows" and "Iosevka SS03,Iosevka Nerd Font:h12"
   or jit.os == "OSX" and "MonaspiceNe Nerd Font:h16"
-  or "Iosevka SS09,Iosevka Nerd Font:h20"
+  or "Iosevka SS09,Iosevka Nerd Font:" .. (env.XDG_SESSION_TYPE == "x11" and "h12" or "h21")
 
 opt.shell = jit.os == "Windows" and "nu" or opt.shell
 opt.shellcmdflag = jit.os == "Windows" and "-c" or opt.shellcmdflag
